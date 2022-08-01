2d9511421e3496ce9bddd91d72571a0a545d759f
HannaKobelska <anna.kobelskaya@gmail.com>
2022-08-01 09:18:52 +0300

    04

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
+import './App.css';
 
 import TaskTitle from './TaskTitle/TaskTitle';
+
 import Profile from './Profile/Profile';
 import user from './Profile/user.json';
 
@@ -9,10 +11,13 @@ import data from './Statistics/data.json';
 import FriendList from './FriendListCont/FriendList';
 import friends from './FriendListCont/friends.json';
 
+import TransactionHistory from './Transaction/TransactionHistory';
+import items from './Transaction/transactions.json';
+
 function App() {
   return (
-    <div>
-    
+    <div className="App">
+      
       <TaskTitle tasktitle="1 - Профиль социальной сети" />
       
       <Profile
@@ -23,15 +28,17 @@ function App() {
           stats={user.stats}
       />
       
-       <TaskTitle tasktitle="2 - Секция статистики" />
+      <TaskTitle tasktitle="2 - Секция статистики" />
      
-       <Statistics title="Upload stats" stats={data} />
+      <Statistics title="Upload stats" stats={data} />
     
       <TaskTitle tasktitle="3 - Список друзей" />
       
       <FriendList friends={friends} />
 
-       <TaskTitle tasktitle="4 - История транзакций" />
+      <TaskTitle tasktitle="4 - История транзакций" />
+
+      <TransactionHistory items={items} />
      
     </div>
 
---
+++ src/components/Transaction/TransactionHistory.js
@@ -0,0 +1,35 @@
+import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+function TransactionHistory({ items }) {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string,
+  amount: PropTypes.string,
+  currency: PropTypes.string,
+};
+
+export default TransactionHistory;
